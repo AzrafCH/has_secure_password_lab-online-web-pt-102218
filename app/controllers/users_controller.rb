51b629c9c310ac031fcfc897968c22476d48f407
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-26 01:38:43 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation, :user_id)
   end
+end
